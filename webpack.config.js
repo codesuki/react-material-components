0fa13e0c72660330681dc0a1a78ba0e9598b4a75
Neri Marschik <codesuki@users.noreply.github.com>
2017-02-03 15:16:50 +0900

    Clean up build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,37 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'react-material-components.js',
-    library: 'ReactMaterialComponents',
-    libraryTarget: 'umd',
-  },
+module.exports = function config(env) {
+  return {
+    entry: './src/index.jsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'react-material-components.js',
+      library: 'ReactMaterialComponents',
+      libraryTarget: 'umd',
+    },
 //  externals: {
   //  react: 'react'
  // },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        include: path.resolve(__dirname, 'src'),
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          include: path.resolve(__dirname, 'src'),
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(env),
+      }),
+      new HtmlWebpackPlugin({ template: 'src/index.html' }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'template/index.html' }),
-  ],
+  };
 };
